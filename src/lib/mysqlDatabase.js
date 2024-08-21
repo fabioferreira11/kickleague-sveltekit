15d24111413ece680a634529913bc69935c8d254
fabioferreira11 <fabio.ferreira.18706@stu.he-ferrer.eu>
2024-08-21 16:24:49 +0200

    changement base de données

--- src/lib/mysqlDatabase.js
+++ src/lib/mysqlDatabase.js
@@ -3,16 +3,16 @@ import mysql from 'mysql2/promise';
 // Configuration de la connexion à la base de données
 const pool = mysql.createPool({
     // //local
-    host: 'localhost',
-    user: 'root',
-    password: '',  
-    database: 'kick_league_db',
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',  
+    // database: 'kick_league_db',
     port: 3307,
     // remote
-    // host: 'localhost',
-    // user: 'uhti7837_fabio',
-    // password: 'L9Ul__C!Fh1%',
-    // database: 'uhti7837_fabio',
+    host: 'localhost',
+    user: 'uhti7837_fabio',
+    password: 'L9Ul__C!Fh1%',
+    database: 'uhti7837_fabio',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
