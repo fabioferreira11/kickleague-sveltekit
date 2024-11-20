d2a72e4615a4736ca34e7153be73009dfe4721f4
fabioferreira11 <fabio.ferreira.18706@stu.he-ferrer.eu>
2024-11-20 16:25:58 +0100

    changement mysqldatabase .netlify

--- .netlify/server/chunks/mysqlDatabase.js
+++ .netlify/server/chunks/mysqlDatabase.js
@@ -1,17 +1,17 @@
 import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   // //local
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "kick_league_db",
-  port: 3307
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "kick_league_db",
+  // port: 3307
   // remote
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  // port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   // waitForConnections: true,
   // connectionLimit: 10,
   // queueLimit: 0
