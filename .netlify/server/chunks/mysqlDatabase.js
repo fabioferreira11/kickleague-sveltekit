4658aad36b7baacf5baac7ba0f4043654cf4f44f
fabioferreira11 <fabio.ferreira.18706@stu.he-ferrer.eu>
2024-11-20 16:19:33 +0100

    test changement mysqldatabse

--- .netlify/server/chunks/mysqlDatabase.js
+++ .netlify/server/chunks/mysqlDatabase.js
@@ -1,19 +1,20 @@
 import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   // //local
-  // host: 'localhost',
-  // user: 'root',
-  // password: '',  
-  // database: 'kick_league_db',
-  // port: 3307,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "kick_league_db",
+  port: 3307
   // remote
-  host: "109.234.160.137",
-  user: "uhti7837_fabio",
-  password: "L9Ul__C!Fh1%",
-  database: "uhti7837_fabio",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // port: process.env.DB_PORT || 3306,
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0
 });
 const mysqlDatabase = {
   async query(sql, params) {
--- src/lib/mysqlDatabase.js
+++ src/lib/mysqlDatabase.js
@@ -14,9 +14,9 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // queueLimit: 0
 });
 
 // Export de l'objet pool pour être utilisé dans d'autres fichiers
