30083202821226816ee88e4c26b4ab71b861354e
fabioferreira11 <fabio.ferreira.18706@stu.he-ferrer.eu>
2024-12-02 19:04:06 +0100

    changement nodemailer

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -21,7 +21,7 @@ export async function POST({ request }) {
         service: process.env.EMAIL_SERVICE || 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            pass: process.env.EMAIL_PASS 
         }
     });
 
